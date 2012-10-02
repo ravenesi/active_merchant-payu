798d76c510530a95f7e79524e4bdfa9dcb4a338e
ravenesi <jurasek.pavel@gmail.com    Delete>
2012-10-02 10:49:59 +0200

    Add czech republic currency and url ..

--- lib/active_merchant-payu.rb
+++ lib/active_merchant-payu.rb
@@ -7,13 +7,13 @@ require 'uri'
 module ActiveMerchant
   module Billing
     class PayuGateway < Gateway
-      BASE_PAYU_URL = "https://www.platnosci.pl/paygw/"
+      BASE_PAYU_URL = "https://www.payu.cz/paygw/"
 
-      self.homepage_url = 'http://www.payu.pl/'
+      self.homepage_url = 'http://www.payu.cz/'
       self.display_name = 'PayU'
       # The countries the gateway supports merchants from as 2 digit ISO country codes
-      self.supported_countries = ['PL']
-      self.default_currency = 'PLN'
+      self.supported_countries = ['CZ']
+      self.default_currency = 'CZK'
       #accepted money format
       self.money_format = :cents
 
